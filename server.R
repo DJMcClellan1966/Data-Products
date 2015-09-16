3bf0cf1695a2f6c9334f4383e3ab30dd36abf988
DJMcClellan1966 <DJMcClellan1966@gmail.com>
2015-09-16 14:58:09 -0400

    Update server.R

--- server.R
+++ server.R
@@ -14,11 +14,17 @@ shinyServer(function(input, output, session) {
   
   output$plot1 <- renderPlot({
     par(mar = c(5.1, 4.1, 0, 1))
+    if (input$plotType == "plot1"){
     plot(selectedData(),
          type = input$plotType,
          col = clusters()$cluster,
          pch = 20, cex = 3)
     points(clusters()$centers, pch = 4, cex = 4, lwd = 4)
+    } else if (input$plotType == "plot2"){
+          plot.default(selectedData(),
+                       type= input$plotType)
+               
+               }
   })
   
   output$summary <- renderPrint({
@@ -26,6 +32,16 @@ shinyServer(function(input, output, session) {
     summary(datasets)
   })
   
+  output$view <- renderTable({
+    head(selectedData(), n=input$obs)
+})
+  
+  output$lm <- renderPrint({
+    model <- lm(input$factor~input$xcol, data= mtcars)
+    summary(model)
+  })
+})
+
   output$view <- renderTable({
     head(selectedData(), n=input$obs)
   })
