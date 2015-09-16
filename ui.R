3038da2d62f56d2066408b79be2c43c17a859c40
DJMcClellan1966 <DJMcClellan1966@gmail.com>
2015-09-15 23:14:11 -0400

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,1 +1,23 @@
+library(shiny)
+library(ggplot2)
 
+
+shinyUI(pageWithSidebar(
+  headerPanel('MTCARS'),
+  sidebarPanel(
+    selectInput('xcol', 'X Variable', names(mtcars)),
+    selectInput('ycol', 'Y Variable', names(mtcars),
+                selected=names(iris)[[2]]),
+    numericInput('clusters', 'Cluster count', 3,
+                 min = 1, max = 31),
+   
+    numericInput("obs", "Number of observations to view:", 10)  
+  ),
+  mainPanel(
+    tabsetPanel(
+    tabPanel("plot", plotOutput('plot1')),
+    tabPanel("Summary", verbatimTextOutput("summary")),
+    tabPanel("Table", tableOutput('view'))
+    )
+  )
+))
