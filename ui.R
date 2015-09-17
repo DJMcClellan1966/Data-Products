96e6093004421381e75ea912d635eab94d03c109
DJMcClellan1966 <DJMcClellan1966@gmail.com>
2015-09-16 21:02:46 -0400

    Update ui.R

--- ui.R
+++ ui.R
@@ -7,7 +7,7 @@ shinyUI(pageWithSidebar(
   sidebarPanel(
     selectInput('xcol', 'X Variable', names(mtcars)),
     selectInput('ycol', 'Y Variable', names(mtcars),
-                selected=names(iris)[[2]]),
+                selected=names(mtcars)[[2]]),
     numericInput('clusters', 'Cluster count', 3,
                  min = 1, max = 31),
    
