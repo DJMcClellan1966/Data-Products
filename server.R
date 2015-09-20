3e3ce99e711c101e00b8378bfe28d5fbdcbbd6b2
DJMcClellan1966 <DJMcClellan1966@gmail.com>
2015-09-20 12:45:23 -0400

    Update server.R

--- server.R
+++ server.R
@@ -1,36 +1,27 @@
+palette(c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3",
+          "#FF7F00", "#FFFF33", "#A65628", "#F781BF", "#999999"))#colors for kmean plots
 library(shiny)
 library(ggplot2)
 library(wordcloud)
 library("corrplot")
 
-
+#define server
 shinyServer(function(input, output, session) {
   
   # Combine the selected variables into a new data frame
+  #selected varials are reactive so should change as different variable selected
   selectedData <- reactive({
-    mtcars[, c(input$xcol, input$ycol)]
+    mtcars[ ,c(input$xcol, input$ycol)]
   })
   factorData <- reactive({
-    mtcars[,c(input$factor)] 
+    mtcars[ ,c(input$factor)]
   })
-split.data <- function(mtcars, p=.75, s= 2015){
-  set.seed(s)
-  index <- sample(1:dim(mtcars)[1])
-  train <- mtcars[index[1:floor(dim(mtcars)[1]*p)], ]
-  test <- mtcars[index[((ceiling(dim(mtcars)[1]*p)) +
-                          1):dim(mtcars)[1]],]
-}  
-  
-all.set <- split.data(mtcars, p=0.7)
-trainset <- all.set$train
-testset <- all.set$test
-
+ 
   clusters <- reactive({
     kmeans(selectedData(), input$clusters)
   })
-  
-  
-  
+    
+#different plot selections and output
   output$plot1 <- renderPlot({
     
     if (input$plotType == "plot1"){
@@ -40,59 +31,41 @@ testset <- all.set$test
            pch = 20, cex = 3)
       points(clusters()$centers, pch = 3, cex = 4, lwd = 4)
     } else if (input$plotType == "plot2"){
-      plot.default(selectedData(),
-                   type= input$plotType
-   ) }  else if(input$plotType == "plot3"){
-      wordcloud(rownames(mtcars), min.freq=1,
-                colors=brewer.pal(7, "Accent"))
-    } else if(input$plotType == "plot4"){
-      hist(x=factorData(),
-           breaks = as.numeric(input$n_breaks),
-           main = "Histogram of factor selction")
-    } else if(input$plotType == "plot5"){
-          pairs(selectedData())
-    } else if(input$plotType =="plot6"){
-      mtscaled <-as.matrix(scale(selectedData()))
-      heatmap(mtscaled,
-              col = topo.colors(200, alpha=0.5),
-              Colv=F, scale="none")
-    } else if(input$plotType == "plot7"){
-      
-      p <- ggplot(factorData(), aes_string(x=input$xcol, y=input$ycol)) + geom_point()
-      
-      if (input$factor != 'None')
-        p <- p + aes_string(color=input$factor)
-      
-      facets <- paste(input$facet_row, '~', input$facet_col)
-      if (facets != '. ~ .')
-        p <- p + facet_grid(facets)
-      
-      if (input$jitter)
-        p <- p + geom_jitter()
-      if (input$smooth)
-        p <- p + geom_smooth()
-      
-      print(p)
-      
+      plot(selectedData(),
+           col = clusters()$cluster
+                   
+      ) }  else if(input$plotType == "plot3"){
+        wordcloud(rownames(mtcars), min.freq=1,
+                  colors=brewer.pal(7, "Accent"))
+      } else if(input$plotType == "plot4"){
+        hist(x=factorData(),
+             breaks = as.numeric(input$n_breaks),
+             main = "Histogram of factor selction")
+      } else if(input$plotType == "plot5"){
+        pairs(selectedData())
+      } else if(input$plotType =="plot6"){
+        mtscaled <-as.matrix(scale(selectedData()))
+        heatmap(mtscaled,
+                col = topo.colors(200, alpha=0.5),
+                Colv=F, scale="none")
+      } 
     
-  
-    }
-   
     
   })
   
+
   output$summary <- renderPrint({
     datasets <- selectedData()
-    summary(datasets)
+    summary(datasets)#summary output of x and y
   })
   
   output$view <- renderTable({
-    head(selectedData(), n=input$obs)
+    head(selectedData(), n=input$obs) #table view of x and y with selected number of objects
   })
   
   output$lm <- renderPrint({
     model <-lm(mpg~ factorData(), mtcars)
     
-    summary(model)
+    summary(model) # linear model of factor vs set mpg variable
   })
 })
