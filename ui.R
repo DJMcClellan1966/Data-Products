52087c210e928d12ffa061500eeabd9db96b8933
DJMcClellan1966 <DJMcClellan1966@gmail.com>
2015-09-20 12:46:34 -0400

    Update ui.R

--- ui.R
+++ ui.R
@@ -2,49 +2,51 @@ library(shiny)
 library(ggplot2)
 
 
+#define the ui
 shinyUI(pageWithSidebar(
   headerPanel('MTCARS'),
+ 
   sidebarPanel(
-    selectInput('xcol', 'X Variable', names(mtcars)),
+    helpText("Create Different plots, summary data, tables, and linear model
+             based on different selections on the mtcars dataset."),
+    selectInput('xcol', 'X Variable', names(mtcars)),#create dropdown to select x
     
-    selectInput('ycol', 'Y Variable', names(mtcars),
+    selectInput('ycol', 'Y Variable', names(mtcars),#create dropdown to select y
                 selected=names(mtcars)[[2]]),
     
-    selectInput('factor', 'Factor', names(mtcars),
+    selectInput('factor', 'Factor', names(mtcars),#create dropdown for variety 
                 selected = names(mtcars)[[3]]),
-    selectInput('color', 'Color', c('None', names(mtcars))),
-    
-    checkboxInput('jitter', 'Jitter'),
-    checkboxInput('smooth', 'Smooth'),
-    
-    selectInput('facet_row', 'Facet Row', c(None='.', names(mtcars))),
-    selectInput('facet_col', 'Facet Column', c(None='.', names(mtcars))),
-    
+   
+  
     selectInput(inputId = "n_breaks",
                 label = "Number of bins in Histogram",
                 choices = c(8,11, 16, 24, 32),
-                selected =  8),
+                selected =  8),# of breaks if hist plot selected
     
     numericInput('clusters', 'Cluster count', 3,
-                 min = 1, max = 31),
+                 min = 1, max = 31),# cluster selection up to 31
     
     numericInput("obs", "Number of observations to view:", 10),
     
     radioButtons("plotType", "Select plot type",
-                 c("plot with kmeans" = "plot1",
-                   "plot without kmeans" = "plot2",
-                   "wordCloud" = "plot3",
-                   "Histogram" = "plot4",
-                   "pairs" = "plot5",
-                   "heatmap" = "plot6",
-                   "ggplot"= "plot7")
-  )),
+                 c("plot with kmeans: x and y, clusters" = "plot1",
+                   "plot without kmeans: x and y " = "plot2",
+                   "wordCloud: no choices" = "plot3",
+                   "Histogram: factor and # bins" = "plot4",
+                   "pairs: x and y" = "plot5",
+                   "heatmap: x and y" = "plot6")#different plot selections
+                   
+    )),
   mainPanel(
     tabsetPanel(
-      tabPanel("plot", plotOutput('plot1')),
-      tabPanel("Summary", verbatimTextOutput("summary")),
-      tabPanel("Table", tableOutput('view')),
-      tabPanel("linearmodel", verbatimTextOutput("lm"))
+      tabPanel("plot", plotOutput('plot1')),#to view plots
+      tabPanel("Summary:
+               choose x and y, number of observations",
+               verbatimTextOutput("summary")),#summary of x and y 
+      tabPanel("Table:
+               choose x and y, number of observations", tableOutput('view')),#table of x and y
+      tabPanel("linearmodel:
+               choose factor vs set mpg", verbatimTextOutput("lm"))#linear model of factor vs mpg
     )
   )
 ))
